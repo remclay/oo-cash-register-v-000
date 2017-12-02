916f24821d0635849f47b9d339c152ffb70429bf
Remmy Clay <remmy.lauren@gmail.com>
2017-12-02 03:17:01 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,8 +8,8 @@ class CashRegister
     @discount = employee_discount
   end
 
-  def add_item(title, price, quantity = nil)
-    @total += price
+  def add_item(title, price, quantity = 1)
+    @total += (price * quantity)
   end
 
 

cfa6e348f49665f10c249423f3e5ff2a588fcfdb
Remmy Clay <remmy.lauren@gmail.com>
2017-12-02 03:43:15 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    (@title << title) * quantity
+    ((@title << title)) * quantity
     @total += (price * quantity)
   end
 
